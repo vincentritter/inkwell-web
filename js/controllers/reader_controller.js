121d88792fa306e7803d87943fbe7aff3eb0415b
Manton Reece <manton@me.com>
2026-01-20 15:51:38 -0600

    Added help text about Fading tab.

--- js/controllers/reader_controller.js
+++ js/controllers/reader_controller.js
@@ -73,6 +73,7 @@ export default class extends Controller {
           <li><code>/</code> — search posts</li>
           <li><code>u</code> — toggle read status</li>
         </ul>
+        <p>What is the <code>Fading</code> tab? Posts older than a few days are collected here. After a week, they are automatically archived, so your unread posts never get out of control.</p>
       </div>
     `;
   }
