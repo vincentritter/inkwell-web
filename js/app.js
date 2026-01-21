fc5d72b55a0ed3985b8946334f6ef1bbbbd69b7f
Manton Reece <manton@me.com>
2026-01-21 11:20:57 -0600

    Added version dates. I don’t love this but I dislike dynamic JS paths more.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 import { Application } from "./stimulus.js";
-import AuthController from "./controllers/auth_controller.js?2";
-import UserMenuController from "./controllers/user_menu_controller.js?2";
-import SessionController from "./controllers/session_controller.js?2";
-import TimelineController from "./controllers/timeline_controller.js?2";
-import ReaderController from "./controllers/reader_controller.js?2";
-import HighlightController from "./controllers/highlight_controller.js?2";
-import HighlightsController from "./controllers/highlights_controller.js?2";
+import AuthController from "./controllers/auth_controller.js?20260121.1";
+import UserMenuController from "./controllers/user_menu_controller.js?20260121.1";
+import SessionController from "./controllers/session_controller.js?20260121.1";
+import TimelineController from "./controllers/timeline_controller.js?20260121.1";
+import ReaderController from "./controllers/reader_controller.js?20260121.1";
+import HighlightController from "./controllers/highlight_controller.js?20260121.1";
+import HighlightsController from "./controllers/highlights_controller.js?20260121.1";
 
 const application = Application.start();
 application.register("auth", AuthController);
