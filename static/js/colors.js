0b7f70d7d156fac3ea5ad4df40de8821d729b019
Manton Reece <manton@me.com>
2026-01-26 08:05:57 -0600

    Changed to just 3 colors.

--- static/js/colors.js
+++ static/js/colors.js
@@ -1,19 +1,39 @@
 export const timelineCellColors = {
-	"day-1": "#CAD4E8",
-	"day-2": "#CDD6EA",
+	"day-1": "#CDD6EA",
+	"day-2": "#E3E8F2",
 	"day-3": "#E3E8F2",
 	"day-4": "#F1F4F8",
-	"day-5": "#F5F7FA",
-	"day-6": "#F8F9FB",
-	"day-7": "#FBFCFD"
+	"day-5": "#F1F4F8",
+	"day-6": "#F1F4F8",
+	"day-7": "#F1F4F8"
 };
 
 export const timelineBorderColors = {
-	"day-1": "#A9B4CF",
-	"day-2": "#BEC7DC",
+	"day-1": "#BEC7DC",
+	"day-2": "#D5DBE7",
 	"day-3": "#D5DBE7",
 	"day-4": "#E3E7EE",
-	"day-5": "#EAEDF3",
-	"day-6": "#F0F2F6",
-	"day-7": "#F4F6F9"
+	"day-5": "#E3E7EE",
+	"day-6": "#E3E7EE",
+	"day-7": "#E3E7EE"
 };
+
+// export const timelineCellColors = {
+	// "day-1": "#CAD4E8",
+	// "day-2": "#CDD6EA",
+	// "day-3": "#E3E8F2",
+	// "day-4": "#F1F4F8",
+	// "day-5": "#F5F7FA",
+	// "day-6": "#F8F9FB",
+	// "day-7": "#FBFCFD"
+// };
+// 
+// export const timelineBorderColors = {
+	// "day-1": "#A9B4CF",
+	// "day-2": "#BEC7DC",
+	// "day-3": "#D5DBE7",
+	// "day-4": "#E3E7EE",
+	// "day-5": "#EAEDF3",
+	// "day-6": "#F0F2F6",
+	// "day-7": "#F4F6F9"
+// };
